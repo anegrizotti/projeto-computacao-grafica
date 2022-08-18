6943ceff2037f721467d60fe19115f760af7116a
Ane <algrizotti@gmail.com>
2022-08-18 10:38:49 -0300

    criado função de desenhar circulo

--- HTMLBase/script.js
+++ HTMLBase/script.js
@@ -3,20 +3,33 @@
 const canvas = document.getElementById("tela");
 const ctx = canvas.getContext("2d");
 
-var 
-x = 200,
-y = 100,
-larg = 300,
-alt = 150;
+function desenharRetangulo() {
+    var 
+    x = 200,
+    y = 100,
+    larg = 300,
+    alt = 150;
 
-function desenhar() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "rgb(255, 128, 0)";
     ctx.fillRect(x, y, larg, alt);
     requestAnimationFrame(desenhar);
 }
 
-requestAnimationFrame(desenhar);
+function desenharCirculo() {
+    var x = canvas.width / 1.5;
+    var y = canvas.height / 1.5;
+    var r = 45;
+
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'rgb(255, 128, 0)';
+    ctx.fill();
+    ctx.stroke();
+}
+
+requestAnimationFrame(desenharRetangulo);
+requestAnimationFrame(desenharCirculo);
 
 document.onkeydown = function (evt) {
     if(evt.keyCode === 38) {
